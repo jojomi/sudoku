d03c47c831f4fee4697e7b97298b5152cd0004c0
jojomi <dev.jojomi@yahoo.com>
2017-12-31 11:32:16 +0100

    Add file reading for UI

--- ui/main.go
+++ ui/main.go
@@ -2,50 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jojomi/sudoku"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	/*s := sudoku.New(2)
-	s.Init([]int{
-		1, 0, 2, 3,
-		0, 0, 4, 0,
-		3, 2, 0, 0,
-		0, 0, 0, 0,
-		//
-		//	1, 4, 2, 3, // 0, 1, 2, 3
-		//	2, 3, 4, 1, // 4, 5, 6, 7
-		//	3, 2, 1, 4, // 8, 9, 10, 11
-		//	4, 1, 3, 2, // 12, 13, 14, 15
-		//
-	})
-	fmt.Println(s)
-	//s.Explain()
-	s.Solve()
-	//s.Solve()
-	fmt.Println(s)*/
-
-	/*s3 := sudoku.New(3)
-	s3.Init([]int{
-		9, 6, 0, 5, 0, 0, 1, 0, 4,
-		0, 0, 0, 4, 0, 0, 5, 6, 0,
-		5, 4, 2, 0, 0, 1, 0, 0, 0,
-		3, 0, 0, 0, 0, 0, 0, 1, 6,
-		0, 0, 1, 9, 0, 7, 0, 0, 5,
-		0, 0, 0, 1, 0, 0, 0, 0, 8,
-		0, 0, 4, 0, 0, 0, 0, 0, 1,
-		0, 0, 0, 0, 2, 3, 4, 5, 7,
-		0, 5, 0, 0, 1, 4, 6, 0, 0,
-	})
-	fmt.Println(s3)
-	for !s3.IsSolved() {
-		s3.Solve()
+	var rootCmd = &cobra.Command{
+		Use: "sudoku filename",
+		Run: cmdSolve,
 	}
-	fmt.Println(s3)*/
 
-	s4 := sudoku.FromFile("test/neuner.sudoku")
-	fmt.Println(s4)
-	s4.Solve()
-	fmt.Println(s4)
+	rootCmd.Execute()
+}
+
+func cmdSolve(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		fmt.Println("Need input filename. Aborting.")
+		os.Exit(1)
+	}
+
+	s := sudoku.FromFile(args[0])
+	fmt.Println("parsed sudoku from input:")
+	fmt.Println(s)
+	s.Solve()
+	fmt.Println("solution:")
+	fmt.Println(s)
 }
